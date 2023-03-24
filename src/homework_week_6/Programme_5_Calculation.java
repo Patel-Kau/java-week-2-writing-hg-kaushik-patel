42cc95d35260feae2d545487d631082ebeef0b79
Patel-Kau <kpatelbaroda@gmail.com>
2023-03-24 10:09:47 +0000

    a

--- src/homework_week_6/Programme_5_Calculation.java
+++ src/homework_week_6/Programme_5_Calculation.java
@@ -1,5 +1,7 @@
 package homework_week_6;
 
+import java.util.Scanner;
+
 /**
  * Write a program for a calculator with addition, subtraction, multiplication and division
  * methods all with parameters and use string concatenation methods.
@@ -10,11 +12,40 @@ public class Programme_5_Calculation {
     // static methods
     public static void addition(int a, int b) {
         int result = a + b;
-        System.out.println(" The addition of " + a + "and" + b + "is : " + result);
+        System.out.println(" The addition of " + a + " and " + b + " is : " + result);
     }
 
     public static int subtraction(int a, int b) {
         return a - b;
     }
+// Instance method
+    public void multiplication (int a, int b){
+        int result = a *b;
+        System.out.println("The multiplication of "+ a+ " and "+b+ " is : "+ result);
+    }
+    public int division (int a, int b){
+        return a/b;
+    }
+
+    public static void main(String[] args) {
+        //Scanner declaration for reading input from console
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter first number : ");
+        int a = scanner.nextInt();
+        System.out.println("Please enter second number : ");
+        int b = scanner.nextInt();
+
+        addition(a,b);
+        int sunResult = subtraction(a,b);
+        System.out.println("The Subtraction of "+a+ " and "+b+ " is : "+ sunResult);
+        Programme_5_Calculation obj = new Programme_5_Calculation();
+        obj.multiplication(a,b);
+        int divResult = obj.division(a,b);
+        System.out.println("The division of "+ a+ " and "+b+ " is : "+ divResult);
+
+        // closing the scanner object
+        scanner.close();
+    }
+
 
 }
